da6c80c76227180acd2de367351220dcdaa7a3f8
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-09 07:47:14 +0000

    unify category & dependencies
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wow_guild/trunk@33056 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'World of Warcraft - Guild',
 	'description' => 'Manage your World of Warcraft Guild',
-	'category' => 'module',
+	'category' => 'World of Warcraft',
 	'author' => 'Jobe',
 	'author_email' => 'jobe@jobesoft.de',
 	'shy' => '',
@@ -32,6 +32,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.0',
 	'constraints' => array(
 		'depends' => array(
+			'cms' => '',
+			'typo3' => '4.3.2',
+			'php' => '5.0',
+			'wow_armory' => '0.0.0',
 		),
 		'conflicts' => array(
 		),
